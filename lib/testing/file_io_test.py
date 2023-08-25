df1c59bdc28d35f1a4c7b7680c27a8d268ccec66
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-08-25 23:00:24 +0300

    2/3 passing

--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -13,13 +13,19 @@ def test_append_file(tmp_path):
     """Test append_file()"""
 
     file_name = tmp_path / "test_file"
-    file_content = "This is a test content."
-    append_content = "\nAppended content."
+    file_content = "This is a test content.\n"
+    append_content = "Appended content."
     write_file(file_name, file_content)
     append_file(file_name, append_content)
     with open(f'{file_name}.txt', 'r') as f:
         file_content_read = f.read()
-    assert file_content_read == file_content + append_content
+
+    normalized_expected = (file_content + '\n' + append_content).replace('\r\n', '\n')
+    normalized_read = file_content_read.replace('\r\n', '\n')
+
+    assert normalized_read == normalized_expected
+
+
 
 def test_read_file(tmp_path):
     """Test read_file()"""
